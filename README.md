822cb4851c325d1ee62e617539871732c1b4cbb9
liumapp <liumapp.com@gmail.com>
2017-11-11 15:41:03 +0800

    add ig

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # session-manager
 基于Spring-data-redis和spring-session，创作的一个springboot模块，用于管理session会话，并存储于Redis中。
+
+## 如何使用
+
+* 启动您的Redis服务
+
---
+++ src/main/java/com/liumapp/config/SessionConfig.java
@@ -0,0 +1,15 @@
+package com.liumapp.config;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * Created by liumapp on 11/10/17.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+@EnableRedisHttpSession
+public class SessionConfig {
+
+    
+
+}
