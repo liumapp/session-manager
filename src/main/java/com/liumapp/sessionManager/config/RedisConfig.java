baa910167a2a1815aa1aa2580e69cdb8fc1531dc
liumapp <liumapp.com@gmail.com>
2017-11-13 14:04:14 +0800

    update redis config

--- src/main/java/com/liumapp/sessionManager/config/RedisConfig.java
+++ src/main/java/com/liumapp/sessionManager/config/RedisConfig.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * Created by liumapp on 11/11/17.
@@ -25,31 +22,11 @@ public class RedisConfig {
         return jedisConnectionFactory;
     }
 
-    @Bean
-    RedisTemplate<String, Object> intRedisTemplate(JedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
-        redisTemplate.setConnectionFactory(connectionFactory);
-        return redisTemplate;
-    }
-
 //    @Bean
-//    ValueOperations<String, Integer> intOperations(RedisTemplate<String, Integer> redisTemplate) {
-//        return redisTemplate.opsForValue();
-//    }
-//
-//    @Bean
-//    RedisTemplate<String, Object> objRedisTemplate(JedisConnectionFactory connectionFactory) {
-//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+//    RedisTemplate<String, String> intRedisTemplate(JedisConnectionFactory connectionFactory) {
+//        RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
 //        redisTemplate.setConnectionFactory(connectionFactory);
-//        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-//        redisTemplate.setKeySerializer(stringRedisSerializer);
-//        redisTemplate.setHashKeySerializer(stringRedisSerializer);
 //        return redisTemplate;
-//    }
-//
-//    @Bean
-//    ValueOperations<String, Object> objOperations(RedisTemplate<String, Object> redisTemplate) {
-//        return redisTemplate.opsForValue();
 //    }
 
 }
